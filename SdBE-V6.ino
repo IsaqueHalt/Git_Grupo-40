ea3b4c6753ceb5cd7b9c369f50fc60a0a3b0a431
IsaqueHalt <143891361+IsaqueHalt@users.noreply.github.com>
2023-12-21 14:55:52 -0300

    Update SdBE-V6.ino

--- SdBE-V6.ino
+++ SdBE-V6.ino
@@ -2,23 +2,23 @@
 |__________________________[Menu do programa]____________________________|
 |       Nome      | Linha | tipo |              Propósito                |
 |_________________|_______|______|_______________________________________|
-|      wi-fi      |  40   |      | Declarar a rede wifi                  |
-|      MQ-135     |  48   |      | Declarar as definições do MQ-135      |
-|       MPU       |  58   |      | Declarar as definições do MPU-6050    |
-|    ThingSpeak   |  40   |      | Informações do canal/campos           |
-|      tempo      |  50   |      | Definir marcas temporais              |
-|       DHT       |  54   |      | Definir pinos e tipo do DHT           | 
-|     LED-RGB     |  59   |      | Definir pinos do LED-RGB              |  
-|     microSD     |  64   |      | Definir o pino CS                     |
-|     mudarCor    |  68   | void | recebe as cores do LED-RGB            |
-|   conectarMQTT  |  83   | void | estabelecer conecção com broker       |
-|     callback    |  102  | void | receber mensagens do broker           |
-|  publishMessage |  115  | void | publicar mensagens em um canal        |
-|     escrever    |  120  | bool | Anexar um valor no arquivo do cartão  |
-|      setup      |  146  | void | inicializar diversos aspectos         |
-|      loop       |  191  | void | rodar o programa continuamente        |
-|                                                                        |     
-|                                                                        |
+|      wi-fi      |  41   |      | Declarar a rede wifi                  |
+|    ThingSpeak   |  45   |      | Informações do canal/campos           |
+|      MQ-135     |  61   |      | Declarar as definições do MQ-135      |
+|       MPU       |  71   |      | Declarar as definições do MPU-6050    |
+|      tempo      |  74   |      | Definir marcas temporais              |
+|       DHT       |  78   |      | Definir pinos e tipo do DHT           | 
+|       KY        |  83   |      | Definir pinos e tipo do KY-037        | 
+|     LED-RGB     |  90   |      | Definir pinos do LED-RGB              |  
+|     microSD     |  96   |      | Definir o pino CS                     |
+|     mudarCor    |  105  | void | recebe as cores do LED-RGB            |
+|       NTP       |  115  |      | Declarar as definições do NTP         |
+|   conectarMQTT  |  120  | void | estabelecer conecção com broker       |
+|     callback    |  142  | void | receber mensagens do broker           |
+|  publishMessage |  155  | void | publicar mensagens em um canal        |
+|     escrever    |  160  | bool | Anexar um valor no arquivo do cartão  |
+|      setup      |  181  | void | inicializar diversos aspectos         |
+|      loop       |  324  | void | rodar o programa continuamente        |
 |________________________________________________________________________|
 
 Boas Festas :D
@@ -40,10 +40,23 @@ Boas Festas :D
 
 //Definições da rede Wi-fi
 const char* SSID = "Intengele_IOT";  
-const char* Senha = "1Nt3n83!3";     
+const char* Senha = "1Nt3n83!3";    
 
-//Definições do Wi-Fi
 WiFiClient espClient;
+
+//Definições do ThingSpeak/Broker
+PubSubClient client(espClient);
+unsigned long canal1 = 1; //DHT22 - Temperatura
+unsigned long canal2 = 1; //DHT22 - Umidade
+unsigned long canal3 = 1; //KY-037 - Decibéis
+const char* apiKey = "POIKW298GF0E0Y8F";
+const char* mqtt_server = "mqtt3.thingspeak.com";
+const char* publishTopic ="channels/2259214/publish"; 
+const char* subscribeTopicFor_Command_1="channels/2259214/subscribe/fields/field1";   //Temperatura 
+const char* subscribeTopicFor_Command_2="channels/2259214/subscribe/fields/field2";   //Umidade
+const char* subscribeTopicFor_Command_3="channels/2259214/subscribe/fields/field3";   //Decibéis
+const char* subscribeTopicFor_Command_4="channels/2259214/subscribe/fields/field4";   //CO2
+const char* subscribeTopicFor_Command_5="channels/2259214/subscribe/fields/field5";   //Vibração
 
 //Definições do MQ-135
 #define placa "ESP-32"
@@ -80,11 +93,12 @@ float potMediaAnt;
 
 //Definições do Adaptador MicroSD
 #define cs 5
+File leituras;
 File Temperatura;
 File Umidade;
 File Decibeis;
-File gCO2;
-File VIC;
+File aCO2;
+File Vib;
 
 //Mudar cor do LED-RGB
 void mudarCor(int vM, int vR, int aZ){
@@ -92,6 +106,7 @@ void mudarCor(int vM, int vR, int aZ){
   analogWrite(pinoVermelho, vM);
   analogWrite(pinoVerde, vR);
   analogWrite(pinoAzul, aZ);
+
 }
 
 //Configurações para aquisição do tempo de um NTP
@@ -99,7 +114,47 @@ const char* ntpServer = "south-america.pool.ntp.org";
 const long  gmtOffset_sec = -10800;      // Fuso horário
 const int   daylightOffset_sec = 0;  // Horário de verão
 
-//Escrever Arquivo no Cartão SD
+//Função para conectar o MQTT
+void conectarMQTT() {
+
+  while (!client.connected()) {
+    Serial.print("Tentando Conectar com o dispositivo MQTT...");
+    //Credenciais do Dispositivo MQTT
+    if (client.connect("EzgxMjsoNhw1JDcLAg4QESk", "EzgxMjsoNhw1JDcLAg4QESk","yH+X7iPdlU+g36SeG/i02vap")) {  
+      Serial.println("MQTT Conectado!");
+      client.subscribe(subscribeTopicFor_Command_1);  //DHT22 - Temperatura
+      client.subscribe(subscribeTopicFor_Command_2);  //DHT22 - Umidade
+      client.subscribe(subscribeTopicFor_Command_3);  //KY-037 - Decibéis
+      client.subscribe(subscribeTopicFor_Command_4);  //MQ-135 - CO2
+      client.subscribe(subscribeTopicFor_Command_5);  //MPU-6050 - Vibração
+      
+    } else {
+      Serial.print("Dispositivo MQTT não foi conectado, erro:");
+      Serial.print(client.state());
+      Serial.println(".");
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String incommingMessage = "";
+  for (int i = 0; i < length; i++) incommingMessage+=(char)payload[i];
+  
+  Serial.println("Message arrived ["+String(topic)+"]"+incommingMessage);
+  
+  //--- check the incomming message
+    if( strcmp(topic,subscribeTopicFor_Command_1) == 0){
+     if (incommingMessage.equals("1")) digitalWrite(BUILTIN_LED, LOW);  
+     else digitalWrite(BUILTIN_LED, HIGH);  
+  }
+}
+
+void publishMessage(const char* topic, String payload , boolean retained){
+  if (client.publish(topic, payload.c_str()))
+      Serial.println("Message publised ["+String(topic)+"]: "+payload);
+}
+
 bool escrever(const char* arquivo, float valor, const char* tipoDeValor) {
     File file = SD.open(arquivo, FILE_APPEND);
 
@@ -120,26 +175,6 @@ bool escrever(const char* arquivo, float valor, const char* tipoDeValor) {
 
     return true;
 }
-
-bool escreverData(const char* arquivo) {
-    File file = SD.open(arquivo, FILE_APPEND);
-
-    if (!file) {
-        return false;
-    }
-    
-    struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
-    Serial.println("Falha ao obter o tempo atual");
-  }
-    file.print("_______________[");
-    file.print(&timeinfo, " %d/%B/%Y ");
-    file.println("]_______________");
-    file.close();
-
-    return true;
-}
-
 
 void setup() {  
   Serial.begin(9600);
@@ -223,7 +258,6 @@ void setup() {
     Serial.println("5 Hz");
     break;
   }
-}
 
   Serial.println("");
   delay(100);
@@ -256,13 +290,17 @@ void setup() {
 
   /*****************************  MQ CAlibration ********************************************/ 
   MQ135.serialDebug(false);
+  
+  //Inicialização das funções MQTT
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 
   //Inicialização do LED
   pinMode(pinoVermelho, OUTPUT);
   pinMode(pinoVerde, OUTPUT);
   pinMode(pinoAzul, OUTPUT);
 
-//Inicialização do Adaptador MicroSD
+  //Inicialização do Adaptador MicroSD
   while (!SD.begin(cs)) {
     Serial.println(F("Cartão com defeito, mal ou não inserido"));
     delay(5000);
@@ -270,7 +308,7 @@ void setup() {
   
   Serial.println(F("Cartão lido com sucesso!"));
 
-  File leituras = SD.open("/leituras.txt", FILE_WRITE);
+ File leituras = SD.open("/leituras.txt", FILE_WRITE);
   if (leituras) {
     leituras.println("Iniciando Leituras");
     leituras.close();
@@ -278,14 +316,7 @@ void setup() {
   } else {
     Serial.println("Não foi possível escrever no arquivo");
   }
-
-  escreverData("/Temperatura.txt");
-  escreverData("/Umidade.txt");
-  escreverData("/Decibeis.txt");
-  escreverData("/gCO2.txt"); 
-  escreverData("/VIC.txt");
-  
-  }
+}
 
 
 void loop() {
@@ -305,17 +336,45 @@ void loop() {
     Serial.print("Rede wi-fi: ");
     Serial.println(WiFi.SSID());
   }
+
+  //Medição da temperatura e umidade
+  //Tempo de medição
+  mudarCor(255,0,255);
+  delay(4000);
+  mudarCor(255,153,255);
+  delay(4000);
+  mudarCor(255,0,255);
+  delay(4000);
+  mudarCor(255,153,255);
+  delay(4000);
+  mudarCor(255,0,255);
+  delay(4000);
   
   //Medição do CO2
   MQ135.update(); // Update data, the arduino will be read the voltage on the analog pin
  
   MQ135.setA(110.47); MQ135.setB(-2.862);
   CO2 = MQ135.readSensor();
+
+  MQ135.setA(605.18); MQ135.setB(-3.937);
+  float CO = MQ135.readSensor();
+
+  MQ135.setA(77.255); MQ135.setB(-3.18);
+  float Alcohol = MQ135.readSensor();
+
+  MQ135.setA(44.947); MQ135.setB(-3.445);
+  float Toluene = MQ135.readSensor();
+
+  MQ135.setA(102.2 ); MQ135.setB(-2.473);
+  float NH4 = MQ135.readSensor();
+
+  MQ135.setA(34.668); MQ135.setB(-3.369);
+  float Acetone = MQ135.readSensor();
   
   CO2 = CO2*12;
 
-  double teste = analogRead(34);
-  Serial.println(teste);
+  Serial.print("CO2: ");
+  Serial.println(CO2);
 
   //Medição da Vibraçao
   sensors_event_t a, g, temp;
@@ -324,7 +383,7 @@ void loop() {
 
   float acT = sqrt((a.acceleration.x)*(a.acceleration.x) + (a.acceleration.y)*(a.acceleration.y) + (a.acceleration.z)*(a.acceleration.z));
   float angT = sqrt((g.gyro.x)*(g.gyro.x) + (g.gyro.y)*(g.gyro.y) + (g.gyro.z)*(g.gyro.z));
-  float vibT = acT*angT;
+  float vibT = 5*acT*angT;
 
   for(int i = 0; i < JANELA; i++){
 
@@ -341,6 +400,11 @@ void loop() {
   }
 
   vibT = vibT/JANELA;
+
+  Serial.print("Vibração = ");
+  Serial.println(vibT);
+
+  Serial.println("");
 
   //Medição do som
   int i;
@@ -382,6 +446,25 @@ void loop() {
 
   potMediaAnt = potMedia;
   potMedia = (potencia + 9 * potMedia)/10;
+  
+
+  if(potencia > 10 * potMediaAnt){
+    Serial.println("----------------------------------------------------------------Evento!----------------------------------------------------------");
+  }
+
+  Serial.print("Media:");
+  Serial.println(media);
+  Serial.print("Maximo:");
+  Serial.println(maximo);
+  Serial.print("Minimo:");
+  Serial.println(minimo);
+  Serial.print("Potencia:");
+  Serial.println(potencia);
+  Serial.print("Potencia Media:");
+  Serial.println(potMedia);
+  Serial.print("Evento:");
+  Serial.println(10*potMedia);
+  Serial.println("");
 
   //Medição de temperatura e umidade
   float u = dht.readHumidity();
@@ -391,35 +474,34 @@ void loop() {
     Serial.println(F("Falha na leitura do Sensor DHT22"));
     return;
   }
-  
-  Serial.println("______________________[Novo Ciclo]______________________");
-
-  Serial.print("Umidade = ");
+  mudarCor(0,255,0);
+  Serial.print(F("Umidade: "));
   Serial.print(u);
-  Serial.println("%");
-
-  Serial.print("Temperatura = ");
+  Serial.print("%");
+  Serial.print(" | ");
+  Serial.print(F("Temperatura: "));
   Serial.print(t);
-  Serial.println("°C");
-
-  Serial.print("Potencia Sonora = ");
+  Serial.print(F("°C"));
+  Serial.print(" | ");
+  Serial.print(F("Intensidade Sonora Relativa: "));
   Serial.print(potencia);
-  Serial.println(" W");
+  Serial.println("");
 
-  Serial.print("CO2 = ");
-  Serial.print(CO2);
-  Serial.println(" ppm");
+  mudarCor(0,0,255);
+  if (!client.connected()) conectarMQTT();
+  client.loop();
 
-  Serial.print("Vibração = ");
-  Serial.print(vibT);
-  Serial.println(" m.rad/s4");
+  if (millis() - ultimoEnvio > intervaloDeEnvio) { // The uploading interval must be > 15 seconds 
+    
+    String dataText = String("field1=" + String(t)+ "&field2=" + String(u) + "&field3=" + String(potencia) + "&field4=" + String(CO2) + "&field5=" + String(vibT) + "&status=MQTTPUBLISH");
+    publishMessage(publishTopic,dataText,true);    
+    ultimoEnvio = millis();
+  }
   
   escrever("/Temperatura.txt", t, "Temperatura");
   escrever("/Umidade.txt", u, "Umidade");
   escrever("/Decibeis.txt", potencia, "Intensidade Sonora");
-  escrever("/gCO2.txt", CO2, "CO2");
-  escrever("/VIC.txt", vibT, "VCI");
+  escrever("/aCO2.txt", CO2, "CO2");
+  escrever("/Vib.txt", vibT, "VCI");
 
-  delay(2500);
-
-}
+ }
